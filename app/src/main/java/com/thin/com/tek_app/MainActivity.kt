1b02160b404122c2c098a873e6a046168f656f55
thin <thineikhaing@gmail.com>
2019-01-24 16:54:20 +0800

    first commit

---
+++ app/src/main/java/com/thin/com/tek_app/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.thin.com.tek_app
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+const val EXTRA_MESSAGE = "com.thin.myfirstapp.MESSAGE"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the send button **/
+    fun sendMessage(view: View){
+        // Do something in response to button
+        val editText = findViewById<EditText>(R.id.editText)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE,message)
+        }
+        startActivity(intent)
+
+    }
+}
