1bf3bf3f007d5c53a4a9bcb50963bfa5da941689
thin <thineikhaing@gmail.com>
2019-01-25 17:43:53 +0800

    getting user location

--- app/src/main/java/com/thin/com/tek_app/MainActivity.kt
+++ app/src/main/java/com/thin/com/tek_app/MainActivity.kt
@@ -1,18 +1,39 @@
 package com.thin.com.tek_app
 
 
+import android.Manifest
+import android.content.Context
+import androidx.core.content.ContextCompat
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
+import androidx.core.app.ActivityCompat
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AlertDialog
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.*
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mDrawerLayout: DrawerLayout
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    private var REQUEST_LOCATION_CODE = 101
+    private var googleApiClient: GoogleApiClient? = null
+    private var locationRequest: LocationRequest? = null
+    private val UPDATE_INTERVAL = (2 * 1000).toLong()  /* 10 secs */
+    private val FASTEST_INTERVAL: Long = 2000 /* 2 sec */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +41,7 @@ class MainActivity : AppCompatActivity() {
 
         var toolbar: Toolbar = findViewById<Toolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+        getSupportActionBar()?.setTitle("Round Trip - SG")
 
         var actionBar: ActionBar? = supportActionBar
         actionBar?.apply{
@@ -39,10 +61,28 @@ class MainActivity : AppCompatActivity() {
             true
         }
 
-    }
+        if (!checkGPSEnabled()){
+            return
+        }
 
-    fun logoClick(view: View){
-        mDrawerLayout.closeDrawers()
+        getLocation();
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations){
+                    // Update UI with location data
+                    // ...
+                    var latitude = location.latitude.toString()
+                    var longitude = location.longitude.toString()
+                    val latLng = latitude + "," + longitude
+                    Log.d("Geo points",latLng)
+                }
+            }
+        }
+
+
+
     }
 
     override fun onOptionsItemSelected(item: android.view.MenuItem?): kotlin.Boolean {
@@ -53,5 +93,75 @@ class MainActivity : AppCompatActivity() {
             }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+
+    fun logoClick(view: View){
+        mDrawerLayout.closeDrawers()
+    }
+
+
+    private fun checkGPSEnabled(): Boolean{
+        if(!isLocationEnabled())
+            showAlert()
+        return isLocationEnabled()
+    }
+
+    private fun isLocationEnabled(): Boolean{
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager!!.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager!!.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun showAlert(){
+        val dialog = AlertDialog.Builder(this )
+        dialog.setTitle("Enable Location")
+            .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " + "use this app")
+            .setPositiveButton("Location Settings") { paramDialogInterface, paramInt ->
+                val myIntent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(myIntent)
+            }
+            .setNegativeButton("Cancel") { paramDialogInterface, paramInt -> }
+        dialog.show()
+    }
+
+    private fun getLocation(){
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+
+        if (fusedLocationClient != null) {
+            Toast.makeText(this, "Location Detected", Toast.LENGTH_SHORT).show();
+
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location : Location? ->
+                    var latitude = location!!.latitude.toString()
+                    var longitude = location!!.longitude.toString()
+                    val latLng = latitude + "," + longitude
+                    Log.d("Geo points ::: ",latLng)
+                    Toast.makeText(this, latLng, Toast.LENGTH_SHORT).show();
+                    // Got last known location. In some rare situations this can be null.
+                }
+
+        }else{
+            Toast.makeText(this, "Location not Detected", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private fun startLocationUpdates() {
+        // Create the location request
+        locationRequest =  LocationRequest.create()?.apply {
+            interval = UPDATE_INTERVAL
+            fastestInterval = FASTEST_INTERVAL
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+        // Request location updates
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return
+        }
+
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+            locationCallback,
+            null /* Looper */)
+
     }
 }
