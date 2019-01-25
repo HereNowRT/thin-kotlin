abc1e5ffafb2c9597f6df3e9a652c41fd825658c
thin <thineikhaing@gmail.com>
2019-01-25 13:54:13 +0800

    Drawer Layout | ToolBar | ActionBar

--- app/src/main/java/com/thin/com/tek_app/DisplayMessageActivity.kt
+++ app/src/main/java/com/thin/com/tek_app/DisplayMessageActivity.kt
@@ -10,11 +10,11 @@ class DisplayMessageActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display_message)
         // Get the Intent that started this activity and extract the string
-        val message =  intent.getStringExtra(EXTRA_MESSAGE)
+//        val message =  intent.getStringExtra(EXTRA_MESSAGE)
 
         //Capture the layout's TextView and set the string as its text
-        val textView = findViewById<TextView>(R.id.textView).apply {
-            text = message
-        }
+//        val textView = findViewById<TextView>(R.id.textView).apply {
+//            text = message
+//        }
     }
 }
--- app/src/main/java/com/thin/com/tek_app/MainActivity.kt
+++ app/src/main/java/com/thin/com/tek_app/MainActivity.kt
@@ -5,25 +5,54 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.support.v4.widget.DrawerLayout
+import android.support.v4.view.GravityCompat
+import android.support.v7.widget.Toolbar
+import android.support.v7.app.ActionBar
+import android.support.design.widget.NavigationView
 
-const val EXTRA_MESSAGE = "com.thin.myfirstapp.MESSAGE"
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mDrawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var toolbar: Toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        var actionBar: ActionBar? = supportActionBar
+        actionBar?.apply{
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+
+            true
+        }
     }
 
-    /** Called when the user taps the send button **/
-    fun sendMessage(view: View){
-        // Do something in response to button
-        val editText = findViewById<EditText>(R.id.editText)
-        val message = editText.text.toString()
-        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
-            putExtra(EXTRA_MESSAGE,message)
+    override fun onOptionsItemSelected(item: android.view.MenuItem?): kotlin.Boolean {
+        return when (item?.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
         }
-        startActivity(intent)
-
     }
 }
